d4b9d3d1c4f3395981733ff67ee0e81f00896b85
naoya kurihara <kuriharanaoya@gmail.com>
2016-09-22 08:10:51 +0900

    tweak scene

---
+++ Assets/Scripts/AgentSpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentSpawner : MonoBehaviour
+{
+    [SerializeField]
+    float interval;
+
+    [SerializeField]
+    GameObject agentPrefab;
+
+
+    float counter = 0.0f;
+
+    void Start ()
+    {
+
+    }
+
+    void Update ()
+    {
+        counter += Time.deltaTime;
+        if( interval <= counter )
+        {
+            var position = transform.position;
+            position.x += Random.Range(-1.5f, 2.0f);
+            var agent = Instantiate(agentPrefab, position, Quaternion.identity) as GameObject;
+            agent.GetComponent<Agent>().wayType = (Agent.WayType)Random.Range(0, 2);
+
+
+            counter = 0.0f;
+        }
+    }
+}
