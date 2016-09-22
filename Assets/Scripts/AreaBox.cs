2412af57e22726bbccff09deeb8e9eae69339ee0
naoya kurihara <kuriharanaoya@gmail.com>
2016-09-22 16:49:18 +0900

    Add Area box for counting agents.

---
+++ Assets/Scripts/AreaBox.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AreaBox : MonoBehaviour
+{
+    List<Agent> agents = new List<Agent>();
+
+    public int NumberOfAgents
+    {
+        get
+        {
+            return agents.Count;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        var agent = col.GetComponent<Agent>();
+        if( null == agent )
+        {
+            return;
+        }
+
+        agents.Add(agent);
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        var agent = col.GetComponent<Agent>();
+        if( null == agent )
+        {
+            return;
+        }
+
+        agents.Remove(agent);
+    }
+}
